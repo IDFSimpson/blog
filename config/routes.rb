89a0d9d7209582e1d37fc8c2413684751c989e3e
Webdev <Ian Simpson>
2016-04-06 22:48:08 -0700

    Added about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get "/home" => "home#home"
 
+  get "/about" => "home#about"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
