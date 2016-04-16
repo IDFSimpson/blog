9546afc3b44b12ea22b0e2a6dd1e48b78e5b39f3
Webdev <Ian Simpson>
2016-04-15 18:05:10 -0700

    refectored rspec test for post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,48 +4,46 @@ RSpec.describe Post, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
 
   describe "validations" do
+    def valid_attributes
+      {title: "Real Title", body: "A body goes here"}
+    end
+
     it "requires a title" do
-      # p0 = FactoryGirl.create(:post, title: nil)
-      p0 = Post.new(title: nil)
+      p0 = Post.create(valid_attributes.merge title: nil)
       p0.valid?
       expect(p0.errors).to have_key(:title)
     end
 
     it "must have a title longer than 7 characters" do
-      # p0 = FactoryGirl.create(:post, title: "1234")
-      p0 = Post.new(title: "1234")
+      p0 = Post.new(valid_attributes.merge(title: "123"))
       p0.valid?
       expect(p0.errors).to have_key(:title)
 
     end
 
     it "requires a unique title" do
-      # p0 = FactoryGirl.create(:post, title: "same_title")
-      # p0.save
-      Post.create(title: "same_title", body: "df")
-      # p1 = FactoryGirl.create(:post, title: "same_title")
-      p1 = Post.new(title: "same_title", body: "af")
+      Post.create(valid_attributes)
+      p1 = Post.new(valid_attributes)
       p1.valid?
-      expect(p1.errors).to have_key :title
 
+      expect(p1.errors).to have_key :title
     end
 
     it "requires a body" do
-      # p0 = FactoryGirl.create(:post, body: nil)
-      p0 = Post.new(body: nil)
+      p0 = Post.new valid_attributes.merge body: nil
       p0.valid?
       expect(p0.errors).to have_key(:body)
     end
 
     it "must have a method, body_snippet, that returns a shortened body" do
       body = Faker::Lorem.characters(99)
-      p0 = FactoryGirl.create(:post, body: "#{body}abcd")
+      p0 = Post.new(title:"title", body: "#{body}abcd")
       expect(p0.body_snippet).to eq("#{body}...")
     end
 
     it "must have a method, body_snippet, that doesn't change small body strings " do
       body = Faker::Lorem.characters(99)
-      p0 = FactoryGirl.create(:post, body: body)
+      p0 = Post.new(title:"title", body: body)
       expect(p0.body_snippet).to eq(body)
     end
 
