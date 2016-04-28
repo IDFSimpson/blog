ff5079170733f744b9c23584bb772c11757ce7ea
Webdev <Ian Simpson>
2016-04-27 18:28:41 -0700

    add rspec test for users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-  # describe "#new" do
-  #   get :new
-  # end
+  describe "#new" do
+    it "renders the new template" do
+      get :new
+      expect(response).to render_template(:new)
+    end
+
+    it "assigns a new user variable" do
+      get :new
+      expect(assigns(:user)).to be_a_new(User)
+    end
+  end
 
   describe "#create" do
     def valid_request
@@ -12,20 +20,46 @@ RSpec.describe UsersController, type: :controller do
     end
 
     describe "with valid attributes" do
-      it "adds user to session"
 
-      it "redirects_to the root path" do
+      it "redirects to the home page" do
+        valid_request
+        expect(response).to redirect_to(root_path)
+      end
+
+      it "adds the new user to session" do
+        valid_request
+        expect(session[:user_id]).to eq(User.last.id)
+      end
+
+      it "adds a record to the database" do
+        expect { valid_request }.to change { User.count }.by(1)
+      end
+
+      it "redirects_to the home page" do
         valid_request
         expect(response).to redirect_to root_path
       end
     end
 
     describe "with invalid attributes" do
-      it "doesn't add a user to session"
-
-      it "redirects_to the root path" do
-        expect(response).to render_template :new
+      def invalid_request
+        post :create, user: FactoryGirl.attributes_for(:user).merge(email: nil)
       end
+
+      it "doesn't add a user to session" do
+        invalid_request
+        expect(session[:user_id]).to eq(nil)
+      end
+
+      it "doesn't add a record to the database" do
+        expect { invalid_request }.to change { User.count }.by(0)
+      end
+
+      it "renders the new template" do
+        invalid_request
+        expect(response).to render_template(:new)
+      end
+
     end
   end
 end
