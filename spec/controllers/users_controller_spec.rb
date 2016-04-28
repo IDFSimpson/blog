9f4ad1666cd7655bd31da5fc0c2f787bf4862210
Webdev <Ian Simpson>
2016-04-27 17:53:06 -0700

    unfinished users rspec tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,30 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+  # describe "#new" do
+  #   get :new
+  # end
+
+  describe "#create" do
+    def valid_request
+      post :create, user: FactoryGirl.attributes_for(:user)
+    end
+
+    describe "with valid attributes" do
+      it "adds user to session"
+
+      it "redirects_to the root path" do
+        valid_request
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    describe "with invalid attributes" do
+      it "doesn't add a user to session"
+
+      it "redirects_to the root path" do
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     first_name "MyString"
     last_name "MyString"
-    email "MyString"
+    email "MyString@email.com"
     password_digest "MyString"
   end
 end
